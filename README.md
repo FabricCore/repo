190a63a16aef3e9abf8d02c863ea49a856f0c9ca
siriusmart <siriusmart@tuta.io>
2025-05-19 20:21:14 +0100

    completed build script #3

--- README.md
+++ README.md
@@ -10,6 +10,147 @@
 4. Other packages can only be accessed through public functions.
 5. Internal variables should not be top level.
 
+### Manifest (package.json) Requirements
+
+- Name in package.json must match name in repo.
+- The field **dependencies** must exist, even if there are no dependencies.
+- The field **authors** must have at least one entry.
+- All dependencies must be valid packages on repo.
+
+### Build Guides
+
+To update the package README, run `node build.js` on a local machine.
+
 ## Package Listing
 
-TODO
+<!--listing:begin-->
+### core
+[**Source**](https://github.com/FabricCore/modcore) | [**Download**](https://github.com/FabricCore/modcore/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install core
+```
+
+Core library.
+- Depends on: *[no dependencies]*
+- Java dependencies: *[no dependencies]*
+- Keywords: core
+
+### emojichat
+[**Download**](https://github.com/Siriusmart/emojichat/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install emojichat
+```
+
+Use Discord style emoji in chat.
+- Depends on: **[keep](#keep)**(0.1.0), **[yarn](#yarn)**(0.1.0), **[toggle](#toggle)**(0.1.0), **[yarntogglepass](#yarntogglepass)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: chat
+
+### keep
+[**Source**](https://github.com/FabricCore/modkeep) | [**Download**](https://github.com/FabricCore/modkeep/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install keep
+```
+
+Lazy storage.
+- Depends on: **[core](#core)**(0.1.0), **[yarn](#yarn)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: core, keep, storage
+
+### pully
+[**Source**](https://github.com/FabricCore/pully) | [**Download**](https://github.com/FabricCore/pully/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install pully
+```
+
+Pully package manager.
+- Depends on: **[core](#core)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: package manager
+
+### toggle
+[**Source**](https://github.com/FabricCore/modtoggle) | [**Download**](https://github.com/FabricCore/modtoggle/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install toggle
+```
+
+Togglable services.
+- Depends on: **[keep](#keep)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: core, toggles
+
+### toggle-floppyfly
+[**Download**](https://github.com/Siriusmart/toggle-floppyfly/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install toggle-floppyfly
+```
+
+Floppy's elytra fly.
+- Depends on: **[keep](#keep)**(0.1.0), **[toggle](#toggle)**(0.1.0), **[utils](#utils)**(0.1.0), **[yarnutils](#yarnutils)**(0.1.0), **[yarn](#yarn)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: efly
+
+### utils
+[**Source**](https://github.com/FabricCore/modutils) | [**Download**](https://github.com/FabricCore/modutils/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install utils
+```
+
+Basic utilities.
+- Depends on: *[no dependencies]*
+- Java dependencies: *[no dependencies]*
+- Keywords: core, toggles
+
+### yarn
+[**Source**](https://github.com/FabricCore/yarn.js) | [**Download**](https://github.com/FabricCore/yarn.js/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install yarn
+```
+
+Yarnwrap helper methods.
+- Depends on: *[no dependencies]*
+- Java dependencies: yarnwrap
+- Keywords: core, yarnwrap
+
+### yarntogglepass
+[**Source**](https://github.com/FabricCore/yarntogglepass) | [**Download**](https://github.com/FabricCore/yarntogglepass/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install yarntogglepass
+```
+
+Yarnwrap event passes.
+- Depends on: **[yarn](#yarn)**(0.1.0), **[toggle](#toggle)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: core, yarnwrap
+
+### yarnutils
+[**Source**](https://github.com/FabricCore/yarnutils) | [**Download**](https://github.com/FabricCore/yarnutils/archive/refs/heads/master.zip) | ***by Sirius***
+
+```
+/pully install yarnutils
+```
+
+Yarnwrap helper methods (extra)
+- Depends on: **[yarn](#yarn)**(0.1.0)
+- Java dependencies: *[no dependencies]*
+- Keywords: yarnwrap, utils
+<!--listing:end-->
+
+## Bad Packages
+
+These packages are submitted to repo, but taken down due to package.json requirements. They will be restored once requirements are met.
+
+Note: a package taken down will cause all packages dependent on it to also be taken down.
+
+<!--badpacks:begin-->
+*[no bad packages]*
+<!--badpacks:end-->
---
+++ build.js
@@ -0,0 +1,135 @@
+(async () => {
+    function eq(a, b) {
+        a = Object.entries(a);
+        a.sort();
+        b = Object.entries(b);
+        b.sort();
+        for (let i = 0; i < Math.max(a.length, b.length); i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    function replace(source, tag, content) {
+        let lines = source.split("\n");
+        let begin = `<!--${tag}:begin-->`;
+        let end = `<!--${tag}:end-->`;
+
+        let beginLine, endLine;
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i] == begin) beginLine = i;
+            if (lines[i] == end) endLine = i;
+        }
+
+        if (beginLine == undefined || endLine == undefined)
+            throw new Error(`tag "${tag}" not found`);
+
+        let out = lines
+            .slice(0, beginLine + 1)
+            .concat(content.split("\n"))
+            .concat(lines.slice(endLine));
+        return out.join("\n");
+    }
+
+    const fs = require("fs").promises;
+    let repo = require("./index.json");
+
+    for (let entry of Object.values(repo)) {
+        try {
+            entry.meta = await (await fetch(entry.package)).json();
+        } catch (ignored) {}
+    }
+
+    let badEntries = {};
+    let goodEntries = {};
+
+    let packages = "";
+
+    Object.entries(repo).forEach(([name, entry]) => {
+        if (entry.meta == undefined)
+            badEntries[name] = `Dead package: could not fetch package.json`;
+        if (entry.meta.name != name)
+            badEntries[name] =
+                `Name mismatch: repo name is ${name} but claims it is ${entry.meta.name}`;
+        if (entry.meta.dependencies == undefined)
+            badEntries[name] = `Missing field: dependencies`;
+        if (
+            !Array.isArray(entry.meta.authors) ||
+            entry.meta.authors.length == 0
+        )
+            badEntries[name] = `No authors: package has no authors`;
+        else goodEntries[name] = true;
+    });
+
+    let removedCount = 0;
+    do {
+        Object.keys(goodEntries).forEach((name) => {
+            if (
+                Object.keys(repo[name].meta.dependencies).some(
+                    (dep) => !goodEntries[dep],
+                )
+            ) {
+                delete goodEntries[name];
+                badEntries[name] =
+                    "Missing dependency: one or more dependencies is not on repo";
+                removedCount++;
+            }
+        });
+    } while (removedCount != 0);
+
+    fs.writeFile("./badEntries.json", JSON.stringify(badEntries, null, 2));
+
+    Object.values(repo)
+        .filter((entry) => goodEntries[entry.meta.name])
+        .sort((a, b) => a.meta.name.localeCompare(b.meta.name))
+        .forEach((entry) => {
+            packages += `### ${entry.meta.name}\n`;
+            packages +=
+                `${entry.meta.repository ? `[**Source**](${entry.meta.repository}) | ` : ""}` +
+                `${entry.meta.homepage ? `[**Home page**](${entry.meta.homepage}) | ` : ""}` +
+                `[**Download**](${entry.zip}) | ` +
+                `***by${entry.meta.authors
+                    .slice(0, -1)
+                    .map((s) => ` ${s}`)
+                    .join(
+                        ",",
+                    )}${entry.meta.authors.length > 1 ? " and" : ""} ${entry.meta.authors[entry.meta.authors.length - 1]}***\n`;
+            packages += "\n";
+            packages += `\`\`\`\n/pully install ${entry.meta.name}\n\`\`\`\n`;
+            packages += "\n";
+            packages += entry.meta.description;
+            packages += "\n";
+            packages += `- Depends on: ${
+                eq(entry.meta.dependencies, {})
+                    ? "*[no dependencies]*"
+                    : Object.entries(entry.meta.dependencies)
+                          .map(
+                              ([name, version]) =>
+                                  `**[${name}](#${name})**(${version})`,
+                          )
+                          .join(", ")
+            }\n`;
+            packages += `- Java dependencies: ${(entry.meta.javaDependencies ?? []).length == 0 ? `*[no dependencies]*` : entry.meta.javaDependencies.join(", ")}\n`;
+            if ((entry.meta.keywords ?? []).length != 0)
+                packages += `- Keywords: ${entry.meta.keywords.join(", ")}`;
+            packages += "\n";
+            packages += "\n";
+        });
+
+    let badPackages = "";
+
+    Object.entries(badEntries).forEach(([name, reason]) => {
+        badPackages +=
+            `- ` + repo[name].meta.repository
+                ? `[**${name}**](${repo[name.meta.repository]})`
+                : `**${name}**` + ` (${reason})\n`;
+    });
+
+    if (packages.length == 0) packages = "*[no packages]*";
+    if (badPackages.length == 0) badPackages = "*[no bad packages]*";
+
+    let readme = await fs.readFile("./README.md", "utf8");
+    readme = replace(readme, "listing", packages.trim());
+    readme = replace(readme, "badpacks", badPackages.trim());
+    fs.writeFile("./README.md", readme);
+})();
